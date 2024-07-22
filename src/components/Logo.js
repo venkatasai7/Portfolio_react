543ea8b0237535d5821c30792733894f516000ce
Venkata Sai Kuniganti <venkatasai7677@gmail.com>
2024-07-23 00:07:24 +0530

    first commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,63 +1,64 @@
-import React, { useLayoutEffect, useState } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import '../Styles/Logo.css';
-import AboutMe from './AboutMe';
-
+import React, { useEffect, useState } from 'react'
+import "../Styles/Logo.css"
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 const Logo = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  const [logo, setLogo] = useState("Hola! I am Venkata Sai Kuniganti.");
+    const [logo, setLogo] = useState("Hola! I am Venkata Sai Kuniganti.");
 
-  useLayoutEffect(() => {
+    useEffect(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".aboutcontent",
+          start: "top bottom",
+          end: "top center",
+          scrub: true,
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".aboutcontent",
-        start: "top bottom",
-        end: "top center",
-        scrub: true,
-        onUpdate: (self) => {
-          setLogo(self.progress > 0.5 ? "Venkata Sai Kuniganti" : "Hola! I am Venkata Sai Kuniganti.");
+          onUpdate: (self) => {
+            if (self.progress > 0.5) {
+              setLogo("Venkata Sai Kuniganti");
+            } else { 
+              setLogo("Hola! I am Venkata Sai Kuniganti.");
+            }
+          },
         },
-      },
-    });
-
-    tl.fromTo(
-      ".logo",
-      { y: "50vh", scale: 3, yPercent: -5 },
-      { y: 0, scale: 1, yPercent: -5 }
-    );
-
-    // Cleanup scroll trigger
-    return () => {
-      tl.scrollTrigger.kill();
-    };
-  },);
-
-  return (
-    <div>
-      <div className="nav">
+      });
+  
+      tl.fromTo(
+        ".logo",
+        {
+          y: "50vh",
+          scale:3,
+          yPercent: -5,
+        },
+        {
+          y: 0,
+          scale: 1,
+          yPercent: -5,
+        }
+      );
+    }, []);
+  
+    return (
+    <div >
+        <div className="nav">
         <div className="items-left">
           <a className="a_nav" href="/">ABOUT</a>
-          <a className="a_nav" href="/">RESUME</a>
+          <a className="a_nav"href="/">RESUME</a>
         </div>
         <div className="items-right">
           <a className="a_nav" href="/">MAIL</a>
-          <a className="a_nav" href="/">CONTACT</a>
+          <a className="a_nav" href="/" >contact</a>
         </div>
       </div>
       <div className="logo-logocontainer">
         <h1 className="logo">{logo}</h1>
       </div>
       <div className="logocontainer">
-        {/* Add any additional content here if needed */}
       </div>
-
-      <AboutMe/>
     </div>
-  );
+  )
 }
 
-export default Logo;
+export default Logo
